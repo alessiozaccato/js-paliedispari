0d5b98b3c90286bf73b7bc09095de540bf2579b2
alessiozaccato <alessiozaccato@gmail.com>
2025-01-15 14:55:24 +0100

    SOLVE: palindrome exercise

--- palindromo/main.js
+++ palindromo/main.js
@@ -1,1 +1,15 @@
 //Palidroma Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+let word = prompt("inserisci una parola:").toLowerCase();
+let reversedWord = palindrome();
+
+function palindrome() {
+    return word.split("").reverse().join("");
+}
+
+
+if (word == reversedWord) {
+    console.log(`la parola ${word} è palindroma`);
+} else {
+    console.log(`la parola ${word} non è palindroma`);
+}
